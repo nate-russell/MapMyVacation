9dcc214d022f0e04f862d5f9a6c628c1ee200a99
nate <ntrusse2@illinois.edu>
2017-07-05 23:01:10 -0500

    added google map styles, seperated code, started on slick alternative to imgur iframe

---
+++ map_viz/map.js
@@ -0,0 +1,219 @@
+/**
+ * Created by nate on 7/5/2017.
+ */
+
+
+function googlemapvis(jsonpath) {
+
+
+    var album_div = document.getElementById("album");
+    var album_title = document.getElementById("album_title");
+    var album_text = document.getElementById("album_text");
+    var album_block = document.getElementById("album_block");
+    var album_a = document.getElementById("album_a");
+
+
+// Create the Google Map…
+    var map = new google.maps.Map(d3.select("#map").node(), {
+        styles: [{
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{"color": "#333739"}]
+        }, {
+            "featureType": "landscape",
+            "elementType": "geometry",
+            "stylers": [{"color": "#2ecc71"}]
+        }, {"featureType": "poi", "stylers": [{"color": "#2ecc71"}, {"lightness": -7}]}, {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{"color": "#2ecc71"}, {"lightness": -28}]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{"color": "#2ecc71"}, {"visibility": "on"}, {"lightness": -15}]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{"color": "#2ecc71"}, {"lightness": -18}]
+        }, {"elementType": "labels.text.fill", "stylers": [{"color": "#ffffff"}]}, {
+            "elementType": "labels.text.stroke",
+            "stylers": [{"visibility": "off"}]
+        }, {
+            "featureType": "transit",
+            "elementType": "geometry",
+            "stylers": [{"color": "#2ecc71"}, {"lightness": -34}]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry",
+            "stylers": [{"visibility": "on"}, {"color": "#333739"}, {"weight": 0.8}]
+        }, {"featureType": "poi.park", "stylers": [{"color": "#2ecc71"}]}, {
+            "featureType": "road",
+            "elementType": "geometry.stroke",
+            "stylers": [{"color": "#333739"}, {"weight": 0.3}, {"lightness": 10}]
+        }],
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+
+    d3.selection.prototype.moveToFront = function () {
+        return this.each(function () {
+            this.parentNode.appendChild(this);
+        });
+    };
+    d3.selection.prototype.moveToBack = function () {
+        return this.each(function () {
+            var firstChild = this.parentNode.firstChild;
+            if (firstChild) {
+                this.parentNode.insertBefore(this, firstChild);
+            }
+        });
+    };
+
+// Load the station data. When the data comes back, create an overlay.
+    d3.json(jsonpath, function (data) {
+        // fit the map to the boundaries of all available data points and
+        // ONCE generate google LatLng objects to be re-used repeatedly
+
+        d3.entries(data).sort(function (d) {
+            return d3.descending(d.value.lng);
+        });
+
+        var bounds = new google.maps.LatLngBounds();
+        d3.entries(data).forEach(function (d) {
+            bounds.extend(d.value.lat_lng = new google.maps.LatLng(d.value.lat, d.value.lng));
+        });
+        map.fitBounds(bounds);
+
+        var overlay = new google.maps.OverlayView(),
+            r = 30,
+            m = 6,
+            padding = r * m;
+        // Add the container when the overlay is added to the map.
+        overlay.onAdd = function () {
+            var layer = d3.select(this.getPanes().overlayMouseTarget)
+                .append("svg")
+                .attr('class', 'stations');
+            overlay.draw = function () {
+                var projection = this.getProjection(),
+                    sw = projection.fromLatLngToDivPixel(bounds.getSouthWest()),
+                    ne = projection.fromLatLngToDivPixel(bounds.getNorthEast());
+                // extend the boundaries so that markers on the edge aren't cut in half
+                sw.x -= padding;
+                sw.y += padding;
+                ne.x += padding;
+                ne.y -= padding;
+
+                d3.select('.stations')
+                    .attr('width', (ne.x - sw.x) + 'px')
+                    .attr('height', (sw.y - ne.y) + 'px')
+                    .style('position', 'absolute')
+                    .style('left', sw.x + 'px')
+                    .style('top', ne.y + 'px');
+
+                var marker = layer.selectAll('.marker')
+                    .data(d3.entries(data))
+                    .each(transform)
+                    .each(transformc);
+
+
+                var image = marker.enter().append('svg:image')
+                    .attr('class', 'marker')
+                    .attr("xlink:href", function (d) {
+                        return d.value.img;
+                    })
+                    .attr("height", r)
+                    .attr("width", r)
+                    .attr("style", "outline: thin solid white;")
+
+                    .attr("preserveAspectRatio", "xMidYMid slice")
+                    .attr("clip-path", function (d) {
+                        return "url(#" + d.key + "_clip)";
+                    }) // clip the image
+                    .on("mouseover", function (d) {
+                        d3.select(this)
+                        //.moveToFront()
+                            .transition()
+                            .attr("height", r * m)
+                            .attr("width", r * m)
+
+                        //dd = projection.fromLatLngToDivPixel(d.value.lat_lng)
+                        //.attr('x',dd.x - sw.x - (r * m / 2))
+                        //.attr('y',dd.y - ne.y - (r * m / 2))
+                        //.attr('x',function(d) {
+                        //dd = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                        //return dd.x - sw.x - (r * m / 2);
+                        //})
+                        //.attr('y',function(d) {
+                        //dd = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                        //return dd.y - ne.y - (r * m / 2);
+                        //})
+                    })
+                    .on("mouseout", function (d) {
+                        d3.select(this)
+                            .transition()
+                            .attr("height", r)
+                            .attr("width", r)
+                        //dd = projection.fromLatLngToDivPixel(d.value.lat_lng)
+                        //.attr('x',dd.x - sw.x - (r / 2))
+                        //.attr('y',dd.y - ne.y - (r / 2))
+                        //.attr('x',function(d) {
+                        //dd = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                        //return dd.x - sw.x - (r / 2);
+                        //})
+                        //.attr('y',function(d) {
+                        //dd = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                        //return dd.y - ne.y - (r / 2);
+                        //})
+                    })
+                    .on('click', function (d) {
+                        d3.select(this)
+
+
+                        // Update Text
+                        album_text.textContent = d.key
+                        album_title.textContent = d.key
+
+                        // Update Album Div Clone
+                        album_block.setAttribute('data-id', d.value.albumid)
+                        album_a.setAttribute('href', "//imgur.com/" + d.value.albumid)
+                        console.log('Post Update album_block')
+                        console.log(album_block.outerHTML)
+
+                        // Inject the HTML content
+                        album_div.innerHTML = album_block.outerHTML
+
+                        // Re-Add Embed script that transforms blockquote into iframe
+                        var imported = document.createElement('script')
+                        imported.src = '//s.imgur.com/min/embed.js'
+                        document.body.appendChild(imported)
+                        ;
+
+                    })
+                    .attr('x', function (d) {
+                        d = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                        return d.x - sw.x - (r / 2);
+                    })
+                    .attr('y', function (d) {
+                        d = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                        return d.y - ne.y - (r / 2);
+                    });
+
+                function transform(d) {
+                    d = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                    return d3.select(this)
+                        .attr('x', d.x - sw.x)
+                        .attr('y', d.y - ne.y);
+                }
+
+                function transformc(d) {
+                    d = projection.fromLatLngToDivPixel(d.value.lat_lng);
+                    return d3.select(this)
+                        .attr('cx', d.x - sw.x)
+                        .attr('cy', d.y - ne.y);
+                }
+            };
+        };
+
+        // Bind our overlay to the map…
+        overlay.setMap(map);
+    });
+};
